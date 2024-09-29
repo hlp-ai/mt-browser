cbe899791bb24b11bbf6db7b441450ae896f7804
Xiaofeng Liu <liuxf@hust.edu.cn>
2024-09-29 18:12:51 +0800

    修改取词翻译结果框；

--- chrome/content_scripts.js
+++ chrome/content_scripts.js
@@ -16,30 +16,31 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendMessage) {
         box.style = "position:fixed; left:0; right:0; top:0; bottom:0; z-index:200;";
         db.body.appendChild(box);
 
-        var div = document.createElement("div");
-        div.style =
+        var dialog_box = document.createElement("div");
+        dialog_box.style =
             "width:500px; height:200px; padding:10px; border-radius:5px; background-color:#fff; box-shadow:2px 0 2px #666, -2px 0 2px #666, 0 2px 2px #666, 0 -2px 2px #666; position:absolute; top:50%; margin-top:-100px; left:50%; margin-left:-250px;";
-        box.appendChild(div);
+        box.appendChild(dialog_box);
 
         var h2 = document.createElement("h2");
         h2.innerHTML = "译文";
         h2.style = "line-height:40px; height:40px; font-size:20px; text-align:center; border-bottom:1px solid #ccc; margin:0;";
-        div.appendChild(h2);
+        dialog_box.appendChild(h2);
 
-        var condiv = document.createElement("div");
-        condiv.innerHTML = res;
-        condiv.style =
-            "overflow:auto; line-height:24px; font-size:16px; border-bottom:1px solid #ccc; text-indent: 2rem; padding:5px; height:95px;";
-        condiv.setAttribute('dir', 'ltr');
-        div.appendChild(condiv);
+        var result = document.createElement("div");
+        result.innerHTML = res;
+        result.style =
+            "overflow:auto; line-height:24px; font-size:16px; border-bottom:1px solid #ccc; padding:5px; height:95px;";
+        result.setAttribute('dir', 'ltr');
+        dialog_box.appendChild(result);
 
-        var btn = document.createElement("div");
-        btn.style = "text-align:right; padding:5px; height:45px;";
-        btn.innerHTML = '<button style="background-color:#6495ed; width:20%; min-width:15%; height:36px; padding:2px; float:right;">确定</button>';
-        div.appendChild(btn);
+        var bottom = document.createElement("div");
+        bottom.style = "text-align:right; padding:5px; height:45px;";
+        bottom.innerHTML = '<button style="background-color:#6495ed; width:20%; font-size:18px; min-width:15%; padding:2px; float:right;">确定</button>';
+        dialog_box.appendChild(bottom);
 
-        var qd = btn.children[0];
-        qd.onclick = function () {
+        var ok_btn = bottom.children[0];
+        // 确定按钮处理事件
+        ok_btn.onclick = function () {
             db.body.removeChild(box);
         }
     }
